8070e79e406da7cc3e5f2170e19d4cce93b1b6c9
Alehub369 <lievanoalejandro@gmail.com>
2025-04-09 20:48:13 -0500

    First connection to server and database
    Signed-off-by: Alehub369 <lievanoalejandro@gmail.com>

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,26 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+connectDB().catch(console.dir);
+
+// middleware
+
+app.use(express.static('./public'));
+app.use(express.json());
+
+// routes
+
+app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = 4000
+app.listen(port, ()=>{
+  console.log(`server is listening on port ${port}...`);
+} )
+
---
+++ 03-task-manager/starter/db/connect.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const uri = "mongodb://localhost:27017";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function connectDB() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("CONNECTED TO THE DB...");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+
+module.exports = connectDB
+
